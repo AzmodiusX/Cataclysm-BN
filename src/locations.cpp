dcb757e822256592e35d7ded61ff7521ac1cfd3e
Azmodius_X <azmosan@gmail.com>
2026-07-07 13:40:39 -0700

    Fix locations

--- src/locations.cpp
+++ src/locations.cpp
@@ -372,11 +372,10 @@ int tile_item_location::obtain_cost( const Character &ch, int qty, const item *i
 
 std::string tile_item_location::describe( const Character *ch, const item * ) const
 {
-    map &here = get_map();
-    const auto local = abs_to_map_local( here, pos_ );
-    std::string res = here.name( local );
+    auto &here = MAPBUFFER_REGISTRY.get( dim_ );
+    std::string res = here.name( pos_ );
     if( ch ) {
-        res += std::string( " " ) += direction_suffix( ch->bub_pos().raw(), abs_to_bub( pos_ ).raw() );
+        res += std::string( " " ) += direction_suffix( ch->abs_pos().raw(), pos_.raw() );
     }
     return res;
 }
@@ -497,9 +496,9 @@ bool vehicle_item_location::is_loaded( const item * ) const
     if( !veh->is_loaded() ) {
         return false;
     }
-
-    //Have to check the bounds, the vehicle might be half outside the bubble
-    return get_map().inbounds( veh->mount_to_bubble( veh->get_part_hack( hack_id ).mount ) );
+    auto &here = veh->get_mapbuffer();
+    auto pos = veh->mount_to_abs( veh->get_part_hack( hack_id ).mount );
+    return abs_tile_handle::fetch( here, pos ).has_value();
 }
 
 tripoint_bub_ms vehicle_item_location::bub_pos( const item * ) const
@@ -525,7 +524,6 @@ item_location_type vehicle_item_location::where() const
 detached_ptr<item> vehicle_item_location::detach( item *it )
 {
     const auto part_index = veh->get_part_id_hack( hack_id );
-    const auto item_pos = veh->mount_to_bubble( veh->get_part_hack( hack_id ).mount );
     const auto temperature = storage_temperature();
     detached_ptr<item> ret = part_index >= 0 ? veh->remove_item( part_index, it ) :
                              veh->get_part_hack( hack_id ).remove_item( *it );
@@ -569,7 +567,7 @@ int vehicle_item_location::obtain_cost( const Character &ch, int qty, const item
     const item *obj = cost_split_helper( it, qty );
     int mv = dynamic_cast<const player *>( &ch )->item_handling_cost( *obj, true,
              VEHICLE_HANDLING_PENALTY );
-    mv += 100 * rl_dist( ch.bub_pos(), veh->mount_to_bubble( veh->get_part_hack( hack_id ).mount ) );
+    mv += 100 * rl_dist( ch.abs_pos(), veh->mount_to_abs( veh->get_part_hack( hack_id ).mount ) );
     return mv;
 }
 
@@ -670,7 +668,7 @@ tripoint_abs_ms contents_item_location::abs_pos( const item * ) const
 
 dimension_id contents_item_location::get_dimension( const item * ) const
 {
-    return dimension_id{}; // TODO
+    return container->get_dimension_id();
 }
 
 std::string contents_item_location::describe( const Character *, const item * ) const
